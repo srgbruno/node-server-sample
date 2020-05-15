2eba54f87d4c8855711d32160d658bf7898cf219
Sérgio Bruno <sbda@icomp.ufam.edu.br>
2020-05-15 19:45:02 -0400

    (Mod) Redis client setup; Express host and port to listen

--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const redis = require('redis');
 
 const usersRoutes = require('./routes/users');
 const postsRoutes = require('./routes/posts');
 
 const app = express();
+const redisClient = redis.createClient({
+   host: "redis-server",
+   port: 6379
+});
+redisClient.set('count', 0);
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+   redisClient.get('count', (err, count) => {
+      res.send({
+         success: true,
+         message: 'Welcome to the API!',
+         visits: count
+      });
+      redisClient.set('count', parseInt(count) + 1);
+   });
+})
 app.use('/usuarios', usersRoutes);
 app.use('/posts', postsRoutes);
 
@@ -20,5 +36,7 @@ app.use((err, req, res, next) => {
    });
 });
 
-const PORT = process.env.port || 3000;
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, console.log(`Server started on port ${PORT}`));
