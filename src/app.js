347daf79be4056fc6ca1661f19841ffa68e171fc
Sérgio Bruno <sbda@icomp.ufam.edu.br>
2021-02-08 19:49:30 -0400

    Change welcome message

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
    redisClient.get('count', (err, count) => {
       res.send({
          success: true,
-         message: 'Welcome to the API!',
+         message: 'Hi there, welcome to my new API!',
          visits: count
       });
       redisClient.set('count', parseInt(count) + 1);
